7294d3b22361060ecfe2aeaa76d145ae6563b732
mjLee <audwn0326@gmail.com>
2022-02-17 19:37:31 +0900

    add path case :id

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import YoutubeDetail from './components/youtube-detail/YoutubeDetail.js';
 import YoutubeMain from './components/youtube-main/YoutubeMain.js';
 import YoutubeService from './services/youtube.service.js';
 
-// const app = new App(document.querySelector('#app'));
+export const navigateTo = url => {
+  history.pushState(null, null, url);
+  router();
+};
 const youtubeService = new YoutubeService('AIzaSyDQj5StsLFdWuxkY_sR9VtQWdTT_Qp5kgk');
+
 const router = async () => {
   const routes = [
     { path: '/', page: YoutubeMain },
@@ -15,13 +19,20 @@ const router = async () => {
     if (location.pathname === route.path) {
       currentPage = new route.page({youtubeService});
     }
+    // TODO: 야매 코드.. regex 이용해서 수정 요망
+    const pathArr = location.pathname.split('/');
+    if (route.path.includes(':id') && pathArr[pathArr.length - 1]) {
+      currentPage = new route.page({youtubeService, id: pathArr[pathArr.length - 1]});
+    }
   });
 
   if (currentPage !== undefined) {
     document.querySelector('#app').innerHTML = (await currentPage.render()).innerHTML;
-    console.log(currentPage.render().innerHTML);
   }
 }
+
+window.addEventListener("popstate", router);
+
 window.addEventListener('DOMContentLoaded', () => {
   router();
 });
